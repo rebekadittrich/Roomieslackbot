17fde2db87399f44fb1879dfb046e1de46cd3919
Akos Zsolt Hochrein <akos.hochrein@prezi.com>
2017-08-07 17:51:09 +0200

    defensive programming

--- roomiebot.py
+++ roomiebot.py
@@ -23,68 +23,71 @@ def handle_command(command, channel):
         are valid commands. If so, then acts on the commands. If not,
         returns back what it needs for clarification.
     """
-    response_message = "Where do you stand and where do you want to go?"
-    if command.startswith(FROM_COMMAND):
-        number = len(command.split())
-        if number >= 2:
-            rooms = ['Spline', 'Cat\'s cradle', 'Hypercube', 'Fractal', 'Icosidodecahedron', 'Eames', 'Regina', 'Lovelace',
-                     'Turing', 'Bob', 'Erdos', 'ContentArranger', 'EagleTransition', 'UserCell', 'AbstractElement',
-                     'Please', 'Workshop', 'Geographer', 'Asteroid b-612', 'Elephant', 'Lamplighter', 'Rose', 'Baobab',
-                     'Cinema', 'Totoro', 'Sangaku', 'Donkey Kong', 'Fuji', 'Stretch']
+    response_message = "I didn't quite get that. Can you make your message simpler?"
+    try:
+        if command.startswith(FROM_COMMAND):
+            number = len(command.split())
+            if number >= 2:
+                rooms = ['Spline', 'Cat\'s cradle', 'Hypercube', 'Fractal', 'Icosidodecahedron', 'Eames', 'Regina', 'Lovelace',
+                         'Turing', 'Bob', 'Erdos', 'ContentArranger', 'EagleTransition', 'UserCell', 'AbstractElement',
+                         'Please', 'Workshop', 'Geographer', 'Asteroid b-612', 'Elephant', 'Lamplighter', 'Rose', 'Baobab',
+                         'Cinema', 'Totoro', 'Sangaku', 'Donkey Kong', 'Fuji', 'Stretch']
 
 
-            first_word = ""
-            second_word = ""
-            first_room = ""
-            second_room = ""
-            from_room = ""
-            to_room = ""
-            command1 = str(command)
-            command = str(command)
-            command = re.sub("[^\w]", " ", command).split()
+                first_word = ""
+                second_word = ""
+                first_room = ""
+                second_room = ""
+                from_room = ""
+                to_room = ""
+                command1 = str(command)
+                command = str(command)
+                command = re.sub("[^\w]", " ", command).split()
 
-            min_lemming = 1000000
-            for word in command:
-                for room in rooms:
-                    if Levenshtein.distance(word, room) < min_lemming:
-                        min_lemming = Levenshtein.distance(word, room)
-                        first_word = word
-                        first_room = room
+                min_lemming = 1000000
+                for word in command:
+                    for room in rooms:
+                        if Levenshtein.distance(word, room) < min_lemming:
+                            min_lemming = Levenshtein.distance(word, room)
+                            first_word = word
+                            first_room = room
 
-            indexfrom = command1.index(first_word)
-            rooms.remove(first_room)
-            command.remove(first_word)
+                indexfrom = command1.index(first_word)
+                rooms.remove(first_room)
+                command.remove(first_word)
 
-            min_lemming2 = 1000000
-            for word in command:
-                for room in rooms:
-                    if Levenshtein.distance(word, room) < min_lemming2:
-                        min_lemming2 = Levenshtein.distance(word, room)
-                        second_word = word
-                        second_room = room
+                min_lemming2 = 1000000
+                for word in command:
+                    for room in rooms:
+                        if Levenshtein.distance(word, room) < min_lemming2:
+                            min_lemming2 = Levenshtein.distance(word, room)
+                            second_word = word
+                            second_room = room
 
-            indexto = command1.index(second_word)
+                indexto = command1.index(second_word)
 
-            if indexfrom < indexto:
-                from_room = first_room
-                to_room = second_room
-            elif indexfrom > indexto:
-                from_room = second_room
-                to_room = first_room
+                if indexfrom < indexto:
+                    from_room = first_room
+                    to_room = second_room
+                elif indexfrom > indexto:
+                    from_room = second_room
+                    to_room = first_room
 
-            response = requests.get("https://roomieapp.herokuapp.com/direction/?searchFrom=" + from_room + "&searchTo=" + to_room)
+                response = requests.get("https://roomieapp.herokuapp.com/direction/?searchFrom=" + from_room + "&searchTo=" + to_room)
 
-            desc = response.json()
-            pathdesc = desc["path"]
+                desc = response.json()
+                pathdesc = desc["path"]
 
-            response_message = "The route from " + from_room + " to " + to_room + "\n"
+                response_message = "The route from " + from_room + " to " + to_room + "\n"
 
-            for path in pathdesc:
-                response_message += path
-                response_message += '\n'
-            response_message += ("For more information please visit us at <https://roomieapp.herokuapp.com/?searchFrom=" + from_room + "&searchTo=" + to_room + ">")
-        else:
-            response_message = "Where do you stand and where do you want to go?"
+                for path in pathdesc:
+                    response_message += path
+                    response_message += '\n'
+                response_message += ("For more information please visit us at <https://roomieapp.herokuapp.com/?searchFrom=" + from_room + "&searchTo=" + to_room + ">")
+            else:
+                response_message = "Where do you stand and where do you want to go?"
+    except Exception:
+        pass
     slack_client.api_call("chat.postMessage", channel=channel,
                           text=response_message, as_user=True)
 
